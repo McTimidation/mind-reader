8561fcd99c77f2329cd707ce65a54fd33d423a67
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-22 15:53:37 +0000

    save before attempting pictures

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const nxtBtn = document.getElementById('mainBtn');
+
+let index = 0;
+
+
+const mainTxt = document.getElementById("mainTxt");
+const mainBtn = document.getElementById("mainBtn");
+const subBtn = document.getElementById("subBtn"); 
+const subTxt = document.getElementById("subTxt"); 
+
+let symbols = [" ! "," @ "," # "," $ "," % "," ^ "," & "," * "," + "];
+
 let pages = [
     {
     headTxt: "I can read your mind",
@@ -29,15 +41,6 @@ let pages = [
 
     {
     headTxt: '',
-    // {symbols: ["!","@","#","$","%","^","&","*","+"],
-    // numbers: function() {
-    //     let range = [];
-    //     for (i = 1; i <= 99; i++) {
-    //         range.push(i);
-    //     }   return range;
-    // } 
-    // }
-    
     subTxt: "Find your new number. Note the symbol beside the number",
     mainBtn: "Reveal",
     subBtn: "Reset",
@@ -51,11 +54,49 @@ let pages = [
     } 
 ]
 
-// function range() { 
-//     for (i = 1; i <= 99; i++) {
-//     // return i;
-//     pages[4].headtxt.numbers.push(i);
-//     return pages[4].headTxt.numbers;
-// }
+function  numbers() {
+        
+    let sym = 0;
+        for (i = 0; i <= 99; i++) {
+            para = document.createElement('p');
+            document.getElementById('mainTxt').appendChild(para);
+            para.textContent = i + symbols[sym];
+            sym++;
+            if (sym === 9) {
+                sym = 0;
+            }
+        } 
+    }
 
-// }
+let answer = symbols[0];   
+
+function display() {
+    mainTxt.textContent = pages[index].headTxt;
+    mainBtn.textContent = pages[index].mainBtn;
+    subBtn.textContent = pages[index].subBtn;
+    subTxt.textContent = pages[index].subTxt;
+    if (index === 4) {
+        numbers();
+    } else if (index === 5) {
+        mainTxt.textContent = answer;
+        subTxt.textContent += answer;
+    } 
+    let state = index;
+    console.log('page' + ++state);
+}
+
+function update() {
+    index++;
+    display();
+}
+
+function reset() {
+    index = 0;
+    display();
+}
+
+display();
+
+nxtBtn.addEventListener('click', update);
+
+subBtn.addEventListener('click', reset);
